7732d0ad2fcd32fcbf5a02450e954f57c646800e
Siobhan McCarter <siobhan.mccarter@gmail.com>
2018-03-08 16:15:10 -0800

    Adding some updates to function and test files

--- src/conf_int.R
+++ src/conf_int.R
@@ -1,4 +1,36 @@
 conf_int <- function (data){
-  #`Computes the confidence interval of the mean from a population.
+  # This function takes in an array and outputs the 95% confidence interval of the population mean.
+
+  # Arguments: data: a 1D array of a list of numbers. Non-numerical values result in an error.
+
+  # Returns: interval: a 2-element 1D array indicating the start and end of the 95% confidence interval.
+
+  mean <- mean(data)
+
+  sd <- sd(data)
+
+  n <- length(data)
+
+  lower <-  round((mean - (1.96*(sd/sqrt(n)))),2)
+
+  upper <- round((mean + (1.96*(sd/sqrt(n)))),2)
+
+  conf <- (c(lower, upper))
+
+  return(conf)
 
 }
+
+
+#if isinstance(data, bool):
+  #raise TypeError("Input cannot be a Boolean")
+
+#if isinstance(data, str):
+  #raise TypeError("Input cannot be a string")
+
+#if np.isnan(data).any() == True:
+  #return ValueError("Input cannot contain NA values")
+
+#if len(data) == 0:
+  #raise ValueError("Input cannot be empty")
+
--- tests/testthat/test-conf_int.R
+++ tests/testthat/test-conf_int.R
@@ -1,14 +1,6 @@
 library(testthat)
 context("conf_int test")
 
-conf_int <- function(data){
-  # This function takes in an array and outputs the 95% confidence interval of the population mean.
-
-  # Arguments: data: a 1D array of a list of numbers. Non-numerical values result in an error.
-
-  # Returns: interval: a 2-element 1D array indicating the start and end of the 95% confidence interval.
-}
-
 # Check that output is an array
 test_that("output data type?", {
   data <- c(1, 2, 3, 4)
