6563ef5f69316d5de54f233f9e4655545cb59172
Jomar Anthony Sastrillo <sastrill@alumni.ubc.ca>
2018-03-09 20:36:57 -0800

    Add function contents

--- src/t_test.R
+++ src/t_test.R
@@ -1,5 +1,35 @@
-t_test <- function(data, mean_0){
+t_test <- function(data, mean_0=0){
   #` Calculates a t-statistic of the data relative
   #` to a mean which may be specified.
 
+  # Checks if the input is correct
+  err_msg = 'data must be a numpy array.'
+  if ( !is.vector(data) ){
+    stop(err_msg)
+  }
+
+  # Checks if there are any missing values
+  err_msg <- 'data has missing values.'
+  if ( any(is.nan(data)) ){
+    stop(err_msg)
+  }
+
+  # Check if the data is empty
+  err_msg <- 'data is empty'
+  if ( length(data)<2 ){
+    stop(err_msg)
+  }
+
+  # Compute statistics
+  mu <- mean(data)
+  npts <- length(data)
+
+  ss <- sum( (data - mu)^2 )
+  var <- ss / (npts-1)
+
+  sem <- sqrt( var / npts)
+
+  # t-statistic
+  t <- ( mu - mean_0 ) / sem
+  return(t)
 }
