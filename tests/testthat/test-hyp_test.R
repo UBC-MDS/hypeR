efd8ff82e6fe4028bf08c8a446b6b195e67be15e
Ivan-Despot <32108839+Ivan-Despot@users.noreply.github.com>
2018-03-16 13:33:45 -0700

    Update test-hyp_test.R

--- tests/testthat/test-hyp_test.R
+++ tests/testthat/test-hyp_test.R
@@ -1,14 +1,14 @@
 context("hyp_test test")
 
-test_that("hyp_test is not a string",{
-expect_error(hyp_test(data = "a random string"))
+test_that("hyp_test is not a boolean",{
+expect_error(hyp_test(data = TRUE, mean_0 = 1, alpha = 0.05))
   })
 
 test_that("hyp_test input greater than 0", {
-  data <- c(1, 2, 3, 4)
+  data <- vector()
   mean_0 <- 1
   alpha <- 0.5
-  expect_gt(hyp_test(length(data), mean_0, alpha), 0)
+  expect_error(hyp_test(data, mean_0, alpha), "missing value where TRUE/FALSE needed")
 })
 
 test_that("hyp_test input must not have missing values",{
@@ -23,5 +23,5 @@ test_that("math is correct", {
   alpha <- 0.05
   output <- 0.9829453
   expect_equal(hyp_test(data, mean_0, alpha), output)
-  
+
 })
