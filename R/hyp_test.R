1b990f47cad6e214eb7ce7f4e6dd0086096c9c70
joesdesk <jomar.sastrillo@gmail.com>
2018-03-18 11:18:36 -0700

    corrected hyp_test_function and tests

--- R/hyp_test.R
+++ R/hyp_test.R
@@ -3,20 +3,25 @@
 #' @param data A 1-dimensional array. Non-numerical values result in an error
 #' @param mean_0 The mean (as a number) of the population under the null hypothesis. Default: 0.
 #' @param alpha The threshold for Type I errors as an float.
-#' @return A summary in the form of a string.
+#' @return The p-value
 hyp_test <- function(data, mean_0, alpha){
 
-# determine dof
+  # determine dof
   deg_of_freedom <- (length(data) - 1)
+  if( deg_of_freedom < 2 ){
+    stop("data must not be empty")
+  }
 
-# determine test statistics
-  test_statistic <- (mean(data) - mean_0) / (sqrt(var(data)) / length(data))
+  # determine test statistics
+  test_statistic <- t_test(data, mean_0)
 
-# find critial value
+  # find critial value
   critical_value <- qt(alpha, df = deg_of_freedom)
 
-# calculate p-value
-  p_value <- pt(test_statistic, df = deg_of_freedom)
+  # calculate 2-tailed p-value
+  test_statistic <- abs(test_statistic)
+  p_value <- 2*pt(abs(test_statistic), df = deg_of_freedom, lower.tail=FALSE)
+
 
   if(test_statistic <= critical_value)
     {print("The test statistic is less than the critial value. Therefore, you should reject the null hypothesis.")}
